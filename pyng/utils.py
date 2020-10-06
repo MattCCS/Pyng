4c2737eb81dcaa98a0df62d70ef9980f6df207df
Matthew Cotton <matt@thecottons.com>
2020-10-05 20:08:07 -0400

    - Clarify column ranges

--- pyng/utils.py
+++ pyng/utils.py
@@ -5,6 +5,6 @@ Display utilities
 
 def column_to_timescale_header(col):
     if col >= 3:
-        return "1{}s".format('0' * (col - 3))
+        return "1{0}-9{0}s".format('0' * (col - 3))
     else:
-        return "1{}ms".format('0' * col)
+        return "1{0}-9{0}ms".format('0' * col)
